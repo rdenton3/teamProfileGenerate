f9da405839c336cc5027fa34357a23a092d89b59
Rachel Denton <rdenton3@terpmail.umd.edu>
2022-02-01 20:52:00 -0500

    manager inquirer questions

--- index.js
+++ index.js
@@ -6,6 +6,63 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
 
+// Create an array of questions for user input
+const questions = () => {
+    return inquirer.prompt([{
+        type: 'input',
+        name: 'managerName',
+        message: 'What is the name of the manager?',
+        validate: managerNameInput => {
+            if (managerNameInput) {
+              return true;
+            } else {
+              console.log('Please enter a manager name.');
+              return false;
+            }
+          }
+        },
+        {
+            type: 'input',
+            name: 'managerID',
+            message: 'Please enter a manager ID number.',
+            validate: idInput => {
+                if (idInput) {
+                return true;
+                } else {
+                console.log('Please enter a manager ID number.');
+                return false;
+                }
+            }
+            },
+            {
+            type: 'input',
+            name: 'managerEmail',
+            message: 'Please provide a manager email address.',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a manager email address.');
+                    return false;
+                }
+                }
+                },    
+        {
+            type: 'input',
+            name: 'managerOfficeNum',
+            message: 'Please provide a manager office number',
+            validate: officeInput => {
+                if (officeInput) {
+                  return true;
+                } else {
+                  console.log('Please enter a manager office number.');
+                  return false;
+                }
+              }
+            }
+        ])}
 
-
+        questions().then((data) => {
+            console.log(data)
+        })
 
